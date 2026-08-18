7c7fdfa49abe8680737e3e606c3c9789919a06f9
Nikhil Farand <nikhilfarand@Air-MacBook.local>
2026-08-18 15:20:46 +0530

    bug fixing

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,10 @@ ROOT = Path(__file__).resolve().parents[1]
 SRC = ROOT / "src"
 if str(SRC) not in sys.path:
     sys.path.insert(0, str(SRC))
+
+
+
+# ROOT = Path(__file__).resolve().parents[1]
+# SRC = ROOT / "src"
+# if str(SRC) not in sys.path:
+#     sys.path.insert(0, str(SRC))
